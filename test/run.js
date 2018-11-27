f595cec3e8e940d5a480a85ca2e4e91f47220e5a
mahhov <mahhov1@gmail.com>
2018-11-27 05:58:04 -0800

    move run.js from src dir to test dir

--- test/run.js
+++ test/run.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const {sync} = require('./syncher');
+const {sync} = require('../src/syncher');
 
 sync(path.resolve(__dirname, '../downloads'), 'PLameShrvoeYfp54xeNPK1fGxd2a7IzqU2');

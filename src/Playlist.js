afd6d232379b2c2bf16385b6b09a5bcdf508ba9f
mahhov <mahhov1@gmail.com>
2018-11-28 06:19:30 -0800

    mark _ for Playlist private methods & fields

--- src/Playlist.js
+++ src/Playlist.js
@@ -5,52 +5,52 @@ const $tream = require('bs-better-stream');
 const Video = require('./Video');
 
 class Playlist {
-	constructor(id) {
-		this.id = id;
-		this.pageCache = {};
-	}
+    constructor(id) {
+        this.id_ = id;
+        this.pageCache_ = {};
+    }
 
-	getOverview() {
-		return this.getOverview_().then(overview => {
-			let playlist = overview.items[0];
-			return playlist
-				? {title: playlist.snippet.title, length: playlist.contentDetails.itemCount}
-				: {title: `no playlist with id ${this.id}`, length: 0};
-		});
-	};
+    getOverview() {
+        return this.getOverview_().then(overview => {
+            let playlist = overview.items[0];
+            return playlist
+                ? {title: playlist.snippet.title, length: playlist.contentDetails.itemCount}
+                : {title: `no playlist with id ${this.id_}`, length: 0};
+        });
+    };
 
-	getVideos() {
-		let pages = $tream();
-		let responses = pages
-			.map(this.getPage_.bind(this))
-			.wait()
-		responses
-			.pluck('nextPageToken')
-			.filter(nextPage => nextPage)
-			.to(pages);
-		pages.write('');
-		return responses
-			.pluck('items')
-			.flatten()
-			.pluck('snippet')
-			.map((snippet, i) => new Video(
-				i,
-				snippet.resourceId.videoId,
-				snippet.title,
-				snippet.thumbnails && snippet.thumbnails.default.url));
-	}
+    getVideos() {
+        let pages = $tream();
+        let responses = pages
+            .map(this.getPage_.bind(this))
+            .wait()
+        responses
+            .pluck('nextPageToken')
+            .filter(nextPage => nextPage)
+            .to(pages);
+        pages.write('');
+        return responses
+            .pluck('items')
+            .flatten()
+            .pluck('snippet')
+            .map((snippet, i) => new Video(
+                i,
+                snippet.resourceId.videoId,
+                snippet.title,
+                snippet.thumbnails && snippet.thumbnails.default.url));
+    }
 
-	getOverview_() {
-		return this.overviewCache = this.overviewCache ||
-			axios.get(`${apiUrl}/playlists?part=snippet,contentDetails&id=${this.id}&key=${apiKey}`)
-				.then(response => response.data)
-	}
+    getOverview_() {
+        return this.overviewCache_ = this.overviewCache_ ||
+            axios.get(`${apiUrl}/playlists?part=snippet,contentDetails&id=${this.id_}&key=${apiKey}`)
+                .then(response => response.data)
+    }
 
-	getPage_(page = '') {
-		return this.pageCache[page] = this.pageCache[page] ||
-			axios.get(`${apiUrl}/playlistItems?part=snippet&maxResults=50&pageToken=${page}&playlistId=${this.id}&key=${apiKey}`)
-				.then(response => response.data)
-	}
+    getPage_(page = '') {
+        return this.pageCache_[page] = this.pageCache_[page] ||
+            axios.get(`${apiUrl}/playlistItems?part=snippet&maxResults=50&pageToken=${page}&playlistId=${this.id_}&key=${apiKey}`)
+                .then(response => response.data)
+    }
 }
 
 module.exports = Playlist;

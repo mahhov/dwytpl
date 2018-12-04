b43192ac3f9aace99ad7787dd4ca3dbf2bb13a67
mahhov <mahhov1@gmail.com>
2018-12-04 15:39:26 -0800

    add sample playlist responses

---
+++ resources/samplePlaylistOverviewResponse.js
@@ -0,0 +1,37 @@
+module.exports = {
+    kind: 'youtube#playlistListResponse',
+    etag: '"XI7nbFXulYBIpL0ayR_gDh3eu1k/n9TYk0rcfQYQicf1DRo6o078FQY"',
+    pageInfo: {totalResults: 1, resultsPerPage: 5},
+    items: [{
+        kind: 'youtube#playlist',
+        etag: '"XI7nbFXulYBIpL0ayR_gDh3eu1k/ycXVPM_M7NVw-VIeK9nuslvuSp8"',
+        id: 'OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw',
+        snippet: {
+            publishedAt: '2018-03-06T23:12:03.000Z',
+            channelId: 'UCBR8-60-B28hp2BmDPdntcQ',
+            title: 'Classical Music Playlist',
+            description: '',
+            thumbnails: {
+                medium: {
+                    url: 'https://i9.ytimg.com/s_p/OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw/mqdefault.jpg?sqp=CPSUnOAFir7X7AMGCMjA_NQF&rs=AOn4CLCSlWl3zmQbQDkgoSn5ExJxEhgUzg&v=5a9f2048',
+                    width: 180,
+                    height: 180
+                },
+                standard: {
+                    url: 'https://i9.ytimg.com/s_p/OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw/sddefault.jpg?sqp=CPSUnOAFir7X7AMGCMjA_NQF&rs=AOn4CLAZ5Vva5LZjZcY4U5pmN0N0WtVJkg&v=5a9f2048',
+                    width: 640,
+                    height: 640
+                },
+                maxres: {
+                    url: 'https://i9.ytimg.com/s_p/OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw/maxresdefault.jpg?sqp=CPSUnOAFir7X7AMGCMjA_NQF&rs=AOn4CLDQTZpM8Zpx3Sta_5k0UlAiF5r4fQ&v=5a9f2048',
+                    width: 1200,
+                    height: 1200
+                }
+            },
+            channelTitle: 'YouTube Spotlight',
+            defaultLanguage: 'en',
+            localized: {title: 'Classical Music Playlist', description: ''}
+        },
+        contentDetails: {itemCount: 65}
+    }]
+};

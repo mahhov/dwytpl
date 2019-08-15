2d24fb2643a3bad5dd837a04a05a49b343c58bc5
manuk <mahhov1@gmail.com>
2019-08-15 06:41:34 -0700

    fix typos in youtube.js

--- src/youtube.js
+++ src/youtube.js
@@ -7,10 +7,10 @@ const get = async endpoint => (await axios.get(endpoint)).data;
 const getPlaylistOverview = id =>
     get(`${API_ENDPOINT}/playlists?part=snippet,contentDetails&id=${id}&key=${API_KEY}`);
 
-const getPlaylistPage = (id, page = '') =>
-    get(`${API_ENDPOINT}/playlistItems?part=snippet&maxResults=50&pageToken=${page}&playlistId=${this.id_}&key=${API_KEY}`);
+const getPlaylistPage = (id, page) =>
+    get(`${API_ENDPOINT}/playlistItems?part=snippet&maxResults=50&pageToken=${page}&playlistId=${id}&key=${API_KEY}`);
 
 const getSearch = query =>
     get(`${API_ENDPOINT}/search?part=snippet&maxResults=15&type=video&q=${query}&key=${API_KEY}`);
 
-module.exports = {getOverview, getPage, getSearch};
+module.exports = {getPlaylistOverview, getPlaylistPage, getSearch};

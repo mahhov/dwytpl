04950291d33039db92c78292fec4a2d771791b27
manuk <mahhov1@gmail.com>
2019-08-17 20:07:01 -0700

    minor, optinoal `count` param to youtube.getSearch

--- src/youtube.js
+++ src/youtube.js
@@ -10,7 +10,7 @@ const getPlaylistOverview = id =>
 const getPlaylistPage = (id, page) =>
     get(`${API_ENDPOINT}/playlistItems?part=snippet&maxResults=50&pageToken=${page}&playlistId=${id}&key=${API_KEY}`);
 
-const getSearch = query =>
-    get(`${API_ENDPOINT}/search?part=snippet&maxResults=15&type=video&q=${query}&key=${API_KEY}`);
+const getSearch = (query, count = 15) =>
+    get(`${API_ENDPOINT}/search?part=snippet&maxResults=${count}&type=video&q=${query}&key=${API_KEY}`);
 
 module.exports = {getPlaylistOverview, getPlaylistPage, getSearch};

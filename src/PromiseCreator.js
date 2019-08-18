13414763b7563b7643f8b34631eeb8dd1d03f6c5
manuk <mahhov1@gmail.com>
2019-08-17 20:06:17 -0700

    expand promiseCreator to include .rejected and .resolved fields to synchroniously  set and check proimse status

--- src/PromiseCreator.js
+++ src/PromiseCreator.js
@@ -1,9 +1,27 @@
 class PromiseCreator {
     constructor() {
         this.promise = new Promise((resolve, reject) => {
-            this.resolve = resolve;
-            this.reject = reject;
+            this.resolve_ = resolve;
+            this.reject_ = reject;
         });
+    }
+
+    get determined_() {
+        return this.resolved || this.rejected;
+    }
+
+    resolve() {
+        if (this.determined_)
+            return;
+        this.resolved = true;
+        this.resolve_();
+    }
+
+    reject() {
+        if (this.determined_)
+            return;
+        this.rejected = true;
+        this.reject_();
     }
 }
 

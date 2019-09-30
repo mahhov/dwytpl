4fac1f0a454025cc6494d1fa8508cfad00922927
manuk <mahhov1@gmail.com>
2019-09-30 06:09:49 -0700

    create queryParams method to reuse for api methods in youtube.js

--- src/youtube.js
+++ src/youtube.js
@@ -2,15 +2,18 @@ const axios = require('axios');
 const API_ENDPOINT = 'https://www.googleapis.com/youtube/v3';
 const API_KEY = 'AIzaSyAdkXuGc2f7xJg5FLTWBi2cRUhzAJD-eC0';
 
-const get = async endpoint => (await axios.get(endpoint)).data;
+const get = async (path, queryParamsObj) => (await axios.get(`${API_ENDPOINT}/${path}?${queryParams(queryParamsObj)}`)).data;
 
-const getPlaylistOverview = id =>
-    get(`${API_ENDPOINT}/playlists?part=snippet,contentDetails&id=${id}&key=${API_KEY}`);
+const queryParams = (params = {}) =>
+    Object.entries(params).map(([key, value]) => `${key}=${value}`).join('&');
 
-const getPlaylistPage = (id, page) =>
-    get(`${API_ENDPOINT}/playlistItems?part=snippet&maxResults=50&pageToken=${page}&playlistId=${id}&key=${API_KEY}`);
+const getPlaylistOverview = playlistId =>
+    get('playlists', {part: 'snippet,contentDetails', id: playlistId, key: API_KEY});
 
-const getSearch = (query, count = 15) =>
-    get(`${API_ENDPOINT}/search?part=snippet&maxResults=${count}&type=video&q=${query}&key=${API_KEY}`);
+const getPlaylistPage = (playlistId, pageToken) =>
+    get('playlistItems', {part: 'snippet', maxResults: 50, pageToken, playlistId, key: API_KEY});
+
+const getSearch = (query, maxResults = 15) =>
+    get('search', {part: 'snippet', maxResults, type: 'video', q: query, key: API_KEY});
 
 module.exports = {getPlaylistOverview, getPlaylistPage, getSearch};

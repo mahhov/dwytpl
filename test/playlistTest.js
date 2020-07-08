33d14a47a01d6f62d561667e656e567eacbb9bb8
manuk <mahhov1@gmail.com>
2020-07-08 11:51:51 -0700

    partial test fixes

--- test/playlistTest.js
+++ test/playlistTest.js
@@ -1,15 +1,8 @@
 const dwytpl = require('../src/index');
 
-let playlist = new dwytpl.Playlist('OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw');
+let playlist = new dwytpl.Playlist('RDEMcHoPxTOy3R6SYpiLmJ_e4Q');
 
 playlist.title.then(title => console.log('title', title));
-playlist.title.then(length => console.log('length', length));
+playlist.length.then(length => console.log('length', length));
 
-[
-    'WTOm79kzVyE',
-    'XyDlLfrba6s',
-    '0rgDg96pGn0',
-    '123',
-    '0rgDg96pGn0x',
-    '0rgDg96pGn4',
-].forEach((id, i) => playlist.getPlaylistItemId(id).then(a => console.log(i, a)));
+// playlist.videos.each((v, i) => console.log(i, v));
